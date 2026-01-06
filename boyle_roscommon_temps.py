88b0ae34be043159d2c026b3aed22775989019ff
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 12:53:58 +0000

    Add files via upload

---
+++ boyle_roscommon_temps.py
@@ -0,0 +1,12 @@
+#Python Pygal PLE
+#Author: Carol Cronin
+#Contact: computerscience@oide.ie
+
+#The code displays the average monthly temperature for Boyle and Roscommon Town
+#for the months January to September
+
+import pygal
+
+# Temperature lists
+boyle_temps = [4.7, 4.8, 6.0, 8.1, 10.9, 13.3, 14.7, 14.3, 12.8]
+roscommon_temps = [5.2, 5.3, 6.4, 8.5, 11.3, 13.8, 15.2, 14.8, 13.3]
