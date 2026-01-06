481f1d3e7ae97af248f612ae2982ee53e9b093c5
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 21:00:31 +0000

    Add files via upload

---
+++ sunshine hours.py
@@ -0,0 +1,23 @@
+#Python Pygal PLE
+#Author: Carol Cronin
+#Contact: computerscience@oide.ie
+
+#The code displays the average monthly hours of sunshine
+#from June to November 2025 in four meteorological
+#weather stations in Ireland.
+#Data from CSO.ie
+
+
+months = [
+    "June 2025",
+    "July 2025",
+    "August 2025",
+    "September 2025",
+    "October 2025",
+    "November 2025"
+]
+
+shannon = [153.0, 155.1, 145.6, 150.5, 67.5, 79.3]
+dublin = [157.7, 158.5, 167.2, 130.6, 69.4, 91.7]
+cork = [135.8, 181.4, 138.1, 160.8, 72.3, 78.6]
+casement = [181.1, 155.2, 190.6, 138.3, 67.7, 84.3]
