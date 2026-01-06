8af36c69f81daa21204373ecb5e35c6e76cf77a4
carolcroninoide <carol.cronin@oide.ie>
2026-01-06 13:10:37 +0000

    Add files via upload

---
+++ castlerea_temps.py
@@ -0,0 +1,8 @@
+#Python Pygal PLE
+#Author: Carol Cronin
+#Contact: computerscience@oide.ie
+
+#The code displays the average monthly temperature for Boyle and Roscommon Town
+#for the months January to September
+
+castlerea_temps = [4.9, 5.0, 6.2, 8.2, 11.0, 13.5, 14.8, 14.4, 12.9]
